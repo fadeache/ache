b7ade38d102ac3d57025aa9f518d1b307f72021f
zhang_gm@git <1424834523@qq.com>
2022-01-07 09:34:45 +0800

    feat: 跨域问题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,34 @@
 <script setup>
-import { ref } from 'vue'
+import { onMounted, reactive, ref } from "vue";
+import axios from "axios";
 
-defineProps({
-  msg: String
-})
+const state = reactive({
+  quotations: "",
+  type: "",
+  tags: ["", "success", "info", "warning", "danger"],
+  loading: false,
+});
+onMounted(() => {
+  getTag();
+});
 
-const count = ref(0)
+const getTag = async () => {
+  state.loading = true;
+  state.quotations = await axios.get("api/SweetNothings");
+  state.loading = false;
+  let index = Math.floor(Math.random() * state.tags.length);
+  state.type = state.tags[index];
+};
+
+const count = ref(0);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <div v-loading="state.loading">
+    <el-tag @close="getTag" closable :type="state.type">
+      {{ state.quotations.data }}
+    </el-tag>
+  </div>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    port: 3333, // 打开的端口号
+    open: true, // 运行时打开浏览器
+    proxy: {
+      "^/api": {
+        target: "https://api.lovelive.tools/", //接口的前缀
+        ws: true, //代理websocked
+        changeOrigin: true, //虚拟的站点需要更管origin
+        pathRewrite: {
+          "^/api": "", //重写路径
+        },
+      },
+    },
+  },
+});
