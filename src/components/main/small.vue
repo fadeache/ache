702bb8376bd3e85e793c1dbc53f5b62a74b39ba0
zhang_gm@git <1424834523@qq.com>
2022-03-21 17:10:02 +0800

    feat: 添加日历日程

--- src/components/main/small.vue
+++ src/components/main/small.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { reactive } from "vue";
+import axios from "axios";
+import { computed, onMounted, reactive } from "vue";
 
 const props = defineProps({
   words: Object,
@@ -8,12 +9,52 @@ const props = defineProps({
 const state = reactive({
   value: new Date(),
   activeName: "1",
+  schedules: [],
+});
+
+onMounted(async () => {
+  let res = await axios.get("ache/calendar/get");
+  state.schedules = res.data;
+});
+
+const getSchedules = computed(() => {
+  return function (data) {
+    let theDay = [];
+    state.schedules.find((item) => {
+      if (item.date === data.day) {
+        theDay.push(item);
+      }
+    });
+    return theDay;
+  };
 });
 </script>
 
 <template>
   <div style="padding: 16px">
-    <el-calendar v-model="state.value"></el-calendar>
+    <el-calendar v-model="state.value"
+      ><template #dateCell="{ data }">
+        <el-popover
+          placement="top-start"
+          trigger="click"
+          v-if="getSchedules(data).length"
+          width="auto"
+        >
+          <li v-for="item in getSchedules(data)">
+            {{ item.event
+            }}<span v-if="item.completed" style="float: right">✔️</span>
+          </li>
+          <template #reference>
+            <div>
+              {{ data.day.split("-").slice(2).join("") }}
+            </div>
+          </template>
+        </el-popover>
+        <div v-else>
+          {{ data.day.split("-").slice(2).join("") }}
+        </div>
+      </template></el-calendar
+    >
     <el-collapse
       v-model="state.activeName"
       accordion
